a75dfe579e2d7fd95583368140b20febfbf1a8f2
SaintAlaye <samuelsunday20@gmail.com>
2023-06-08 07:49:46 -0700

    error fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,16 +4,15 @@ from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-
+s1 = add(a, b)
+s2 = sub(a, b)
+s3 = mul(a, b)
+s4 = div(a, b)
 
 def calc():
     """
         function to print the return values
     """
-    s1 = add(a, b)
-    s2 = sub(a, b)
-    s3 = mul(a, b)
-    s4 = div(a, b)
     print("{} + {} = {}".format(a, b, s1))
     print("{} + {} = {}".format(a, b, s2))
     print("{} + {} = {}".format(a, b, s3))
