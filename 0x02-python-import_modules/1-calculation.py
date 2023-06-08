7de0367b5cd74e556344e101c9e90adef1fe1e36
SaintAlaye <samuelsunday20@gmail.com>
2023-06-08 08:46:44 -0700

    errors fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-
 a = 10
 b = 5
-add(a, b)
-sub(a, b)
-mul(a, b)
-div(a, b)
+
 
 def calc():
     """
-        function to print the return values
+        function to print th values
     """
-    print(a, '+', b, '=', add(a, b))
-    print(a, '-', b, '=', sub(a, b))
-    print(a, '*', b, '=', mul(a, b))
-    print(a, '/', b, '=', div(a, b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
 
 if __name__ == "__main__":
