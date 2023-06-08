c36245bffa7e619bac9c38f3c1c4a06eb3760058
SaintAlaye <samuelsunday20@gmail.com>
2023-06-08 07:03:03 -0700

    indention error fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,11 +4,14 @@ from add_0 import add as add_1
 a = 1
 b = 2
 value = add_1(a, b)
+
+
 def print1():
     """
-	    function that print the value
+        function that print the value
     """
     print("{} + {} = {}".format(a, b, value))
+
 
 if __name__ == "__main__":
     print1()
