8ea991543dab7c57b4fb88c830077f87851f962d
SaintAlaye <samuelsunday20@gmail.com>
2023-06-11 18:29:20 -0700

    print new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """ replace element in a list """
+    if idx < 0:
+        return my_list.copy()
+    elif idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        my_list[idx] = element
+        return my_list
