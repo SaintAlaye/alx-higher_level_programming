6fa17e0ccc403773a7f6431dc2b87c62124bf429
SaintAlaye <samuelsunday20@gmail.com>
2023-06-11 17:38:27 -0700

    print index in element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """ function that print at index """
+    if idx < 0:
+        return None
+
+    elif idx > len(my_list):
+        return None
+
+    else:
+        return my_list[idx]
