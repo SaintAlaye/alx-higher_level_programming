90ed80fe168c37d148c9cc178157c032392b51bb
SaintAlaye <samuelsunday20@gmail.com>
2023-06-11 17:49:38 -0700

    replace in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """ printing new element in a list """
+    if idx < 0:
+        return my_list
+
+    elif idx > len(my_list) -1:
+        return my_list
+
+    else:
+        my_list[idx] = element
+        return my_list
