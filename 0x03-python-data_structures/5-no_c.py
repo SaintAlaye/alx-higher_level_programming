d4e4c8164842dd3e64cd870c54ce5e3a01f8fdbb
SaintAlaye <samuelsunday20@gmail.com>
2023-06-12 15:56:49 -0700

    remove strings without using replace()

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 
 def no_c(my_string):
     """ remove a character in a string """
-    for c in my_string:
-        if c == "c" and c == "C":
-            my_string -= c
-            new_string = my_string
-            return new_string
+    my_string = "".join([char for char in my_string if char != "c" and char != "C"])
+    new_string = my_string
+    return new_string
