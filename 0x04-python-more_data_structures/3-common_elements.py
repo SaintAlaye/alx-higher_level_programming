15643f32a80cb0eadf7b05e91f3b9dd635bb1649
SaintAlaye <samuelsunday20@gmail.com>
2023-06-14 08:38:10 -0700

    return common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-   """ return common elements """
-   return (set_1 & set_2)   
+    """ return common elements """
+    return (set_1 & set_2)
